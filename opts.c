aa617c704057746dbe1220b756f0ecdbf8dde4a0
Anton Berezin <tobez@tobez.org>
2013-10-01 15:26:28 +0200

    Update copyright year.

--- opts.c
+++ opts.c
@@ -1,7 +1,7 @@
 /*
  * Part of `snmp-query-engine`.
  *
- * Copyright 2012, Anton Berezin <tobez@tobez.org>
+ * Copyright 2012-2013, Anton Berezin <tobez@tobez.org>
  * Modified BSD license.
  * (See LICENSE file in the distribution.)
  *
